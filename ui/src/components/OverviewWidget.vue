37be2ca0dcfb40460464e74919ee7d6f59383dcd
MFreidank <freidankm@gmail.com>
2019-11-02 17:32:05 +0100

    Add: Widget to give high-level overview
    Rough overview with basic graphing (including WIFI health),
    based on the official opensensemap widget, integrated with a vue-friendly-iframe.
    Planned to be shown on the home screen next to the map that can
    subsequently route to the detailed views.

---
+++ ui/src/components/OverviewWidget.vue
@@ -0,0 +1,24 @@
+<template>
+     <vue-friendly-iframe v-if="url" :src="url"></vue-friendly-iframe>
+</template>
+
+<script>
+import { widgetURL } from "@/config/senseBox";
+export default {
+  name: "OverviewWidget",
+  data: () => ({
+    url: widgetURL,
+  }),
+};
+</script>
+
+<style>
+.vue-friendly-iframe iframe {
+  width: 400px;
+  height: 600px; 
+  hspace: 0px; 
+  vspace: 0px; 
+  frameborder: 0px; 
+  scrolling:"no";
+}
+</style>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <HelloWorld/>
+    <OverviewWidget/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import OverviewWidget from '@/components/OverviewWidget.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    OverviewWidget
   }
 }
 </script>
