6840993a721984003453f9c07b109ff99a7e5242
Tamalera <florence.meier@alumni.ethz.ch>
2019-11-02 14:40:22 +0100

    fix navigation

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <v-app>
+    <nav>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span class="font-weight-light">Testpage</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>
-          <router-link to="/">Home</router-link>
-        </v-btn>
-        <v-btn flat>
-          <router-link to="/about">About</router-link>
-        </v-btn>
+          <v-btn class="mx-1" to="/">Home</v-btn>
+          <v-btn class="mx-2" to="/about">About</v-btn>
       </v-toolbar-items>
     </v-toolbar>
+    </nav>
 
     <v-content>
       <router-view/>
