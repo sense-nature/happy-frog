9f9d88316d161172411f73380322560d50009de5
MFreidank <freidankm@gmail.com>
2019-11-02 23:30:48 +0100

    towards: new interface for sensebox handling
    New interface should compute relevant sensors associated to a given sensebox
    based on the sensebox location that a user showed interest in.

--- ui/src/components/OverviewWidget.vue
+++ ui/src/components/OverviewWidget.vue
@@ -6,8 +6,13 @@
 import { widgetURL } from "@/config/senseBox";
 export default {
   name: "OverviewWidget",
+  props: {
+    // TODO: Use sensebox ID from "@config/senseBox/senseBoxes" and 
+    // pick the sensebox with location closest to where a user clicked. 
+    senseBoxID: {type: String, default: "5dbd75c3a68df4001a658ed8",
+  },
   data: () => ({
-    url: widgetURL,
+    url: widgetURL(senseBoxID),
   }),
 };
 </script>
--- ui/src/config/senseBox.js
+++ ui/src/config/senseBox.js
@@ -1,5 +1,31 @@
+import axios from "axios";
+
+// New interface: multiple sensebox ids, also keep track of their name and location
+export const senseBoxes = [
+  {"id": "5dbd75c3a68df4001a658ed8", "name": "happyfrog01", "location": [7.551, 47.536, 312.6]},
+  {"id": "5dbd82e0a68df4001a69631d", "name": "happyfrog02", "location": [7.55045, 47.53636, 312.1]},
+]
+
+// Old interface: assumes single sensebox
 export const senseBoxID = "5dbd75c3a68df4001a658ed8";
-export const sensorIDs = [
-    "5dbd75c3a68df4001a658edb",
-];
-export const widgetURL = "https://sensebox.github.io/opensensemap-widget/iframe.html?senseboxId=" + senseBoxID + "&initialTab=sensors";
+
+export function widgetURL(senseBoxID) {
+  // Compute the URL for the overview widget for a given sensebox ID.
+  return "https://sensebox.github.io/opensensemap-widget/iframe.html?senseboxId=" + senseBoxID + "&initialTab=sensors";
+};
+
+export function retrieveSensors(senseBoxID) {
+  // Retrieve information for sensors belonging to a given senseBoxID. 
+  // Warning: untested code.
+  axios.get("https://api.opensensemap.org/boxes/" + senseBoxID + "?format=json").then(
+    response => {
+      response.data.sensors.map((sensor) => {
+        name: sensor.title,
+        unit: sensor.unit,
+        coordinates: response.data.currentLocation.coordinates,
+        senseBoxName: response.data.name,
+        senseBoxID: senseBoxID,
+      }
+    }
+  );
+};
