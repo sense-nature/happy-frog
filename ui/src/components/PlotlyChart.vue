416c54699bc61d03cee4659bf46a0aee38bada6c
MFreidank <freidankm@gmail.com>
2019-11-02 23:30:05 +0100

    plotly chart for single sensor and senseboxid

--- ui/src/components/PlotlyChart.vue
+++ ui/src/components/PlotlyChart.vue
@@ -11,6 +11,7 @@
 <script>
 import VuePlotly from "@statnett/vue-plotly";
 import axios from "axios";
+import { senseBoxID } from "@/config/senseBox";
 export default {
   name: "PlotlyChart",
   components: {
@@ -18,51 +19,56 @@ export default {
   },
 
   props: {
-    // Can be adapted to process other sensors.
-    sensorID: {type: String, default: "5dbd75c3a68df4001a658edb"},
+    // TODO: Use sensebox ID from "@config/senseBox/senseBoxes" and 
+    // pick the sensebox with location closest to where a user clicked. 
+    // Then, retrieve the corresponding sensors to plot information for using
+    // "@config/senseBox/retrieveSensors". 
+    senseBoxID: {type: String, default: "5dbd75c3a68df4001a658ed8",
+    sensorID: {type: String, default: "5dbd82e0a68df4001a69631f"},
   },
 
   data: function() {
     return {
       data: [],
+      senseBoxName: "",
+      sensorType: "",
+      measurementUnit: "",
       layout: {
-        xaxis: {
-          title: "Date",
-          autorange: true,
-          range: ['2019-02-17', '2019-12-16'],
-          rangeselector: {buttons: [
-              {
-                count: 1,
-                label: '1d',
-                step: 'day',
-                stepmode: 'backward'
-              },
-              {
-                count: 1,
-                label: '1m',
-                step: 'month',
-                stepmode: 'backward'
-              },
-              {step: 'all'}
-            ]},
-          type: 'date'
-        },
-
-        yaxis: {
-          title: "Temperature °C",
-        },
-
-     },
-   options: {
-     responsive: true,
-     showLink: false,
-     displayModeBar: false,
-   },
+          xaxis: {
+            title: "Date",
+            autorange: true,
+            range: ['2019-02-17', '2019-12-16'],
+            rangeselector: {buttons: [
+                {
+                  count: 1,
+                  label: '1d',
+                  step: 'day',
+                  stepmode: 'backward'
+                },
+                {
+                  count: 1,
+                  label: '1m',
+                  step: 'month',
+                  stepmode: 'backward'
+                },
+                {step: 'all'}
+              ]},
+            type: 'date'
+          },
+          yaxis: {
+            title: "Temperature °C",
+          },
+       },
+      options: {
+        responsive: true,
+        showLink: false,
+        displayModeBar: false,
+      },
    };
   },
 
   mounted() {
-    axios.get("https://api.opensensemap.org/boxes/5dbd75c3a68df4001a658ed8/data/" + this.sensorID).then(
+    axios.get("https://api.opensensemap.org/boxes/" + senseBoxID + "/data/" + this.sensorID).then(
       response => (this.data = [{
         x: response.data.map((row) => row.createdAt),
         y: response.data.map((row) => row.value), 
