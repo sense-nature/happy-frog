a58c8a449869284b2dd6b4944f1b572b4bb2983b
nonameleftforme <taty.vogel@gmx.ch>
2019-11-02 16:17:11 +0100

    Add svg-map

--- ui/src/components/HelloWorld.vue
+++ ui/src/components/HelloWorld.vue
@@ -8,16 +8,22 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Pond Overview  
         </h1>
+        <Map class="map"/>
       </v-flex>
       <v-flex>
         <v-btn to="/detail">Detail</v-btn>
       </v-flex>
+
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import Map from './map.vue';
 export default {
+  components: {
+    Map,
+  },
   data: () => ({
     
   }),
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
 }
