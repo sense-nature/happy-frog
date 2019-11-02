1b78a83bef1d93fe75cb5bb0b4ef6edebe172b78
Tamalera <florence.meier@alumni.ethz.ch>
2019-11-02 17:06:03 +0100

    linting only

--- ui/src/components/HelloWorld.vue
+++ ui/src/components/HelloWorld.vue
@@ -1,48 +1,33 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
+    <v-layout text-center wrap>
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
-          Pond Overview  
+          Pond Overview
         </h1>
       </v-flex>
       <v-flex>
-        <v-dialog
-      v-model="dialog"
-      width="90vw"
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn v-on="on">Detail</v-btn>
-      </template>
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          PONDY the happiest pond in west pond cluster
-        </v-card-title>
+        <v-dialog v-model="dialog" width="90vw">
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on">Detail</v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="headline grey lighten-2" primary-title>
+              PONDY the happiest pond in west pond cluster
+            </v-card-title>
 
-        <v-card-text>
-          HERE IS AN AMAZING CHART
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+            <v-card-text>
+              HERE IS AN AMAZING CHART
+            </v-card-text>
+          </v-card>
+        </v-dialog>
       </v-flex>
-    <v-snackbar
-    color="error"
-        v-model="snackbar"
-        >
+      <v-snackbar color="error" v-model="snackbar">
         <strong>{{ text }}</strong>
-        <v-btn
-        text
-          @click="snackbar = false"
-        >
+        <v-btn text @click="snackbar = false">
           Close
         </v-btn>
-    </v-snackbar>
+      </v-snackbar>
     </v-layout>
   </v-container>
 </template>
@@ -51,12 +36,12 @@
 export default {
   data: () => ({
     snackbar: false,
-    text: "ALERT: Pondy is too hot",
+    text: "ALERT: Pondy is too hot"
   }),
-  mounted: function () {
-  this.$nextTick(function () {
-    setInterval(() => this.snackbar = true, 10000);
-  })
-}
+  mounted: function() {
+    this.$nextTick(function() {
+      setInterval(() => (this.snackbar = true), 10000);
+    });
+  }
 };
 </script>
