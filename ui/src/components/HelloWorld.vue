fc3c929a445ed52b08320fb50460fd1f6b7be2b1
Tamalera <florence.meier@alumni.ethz.ch>
2019-11-02 18:10:27 +0100

    Modal opens on click of pond

--- ui/src/components/HelloWorld.vue
+++ ui/src/components/HelloWorld.vue
@@ -2,16 +2,11 @@
   <v-container>
     <v-layout text-center wrap>
       <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Pond Overview
-        </h1>
-        <Map class="map"/>
+        <Map class="map" />
       </v-flex>
       <v-flex>
         <v-dialog v-model="dialog" width="90vw">
-          <template v-slot:activator="{ on }">
-            <v-btn v-on="on">Detail</v-btn>
-          </template>
+          <template v-slot:activator="{ on }"> </template>
           <v-card>
             <v-card-title class="headline grey lighten-2" primary-title>
               PONDY the happiest pond in west pond cluster
@@ -34,10 +29,10 @@
 </template>
 
 <script>
-import Map from './map.vue';
+import Map from "./map.vue";
 export default {
   components: {
-    Map,
+    Map
   },
   data: () => ({
     snackbar: false,
