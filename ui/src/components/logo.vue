011cbb76d14a568fb2693143e46ffbd5df4e5685
Pablo Escodebar <escodebar@gmail.com>
2019-11-03 14:52:56 +0100

    Remove UI folder

--- ui/src/components/logo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <svg id="Ebene_5" data-name="Ebene 5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 282.68 127.9"><title>logo02</title><rect class="cls-1" y="23.34" width="218.26" height="87.75"/><rect class="cls-2" x="73.56" y="40.14" width="184.09" height="87.75"/><rect class="cls-2" x="44.62" width="238.06" height="87.75"/></svg>
-</template>
-
-<style scoped>.cls-1{fill:#145ca8;}.cls-1,.cls-2{opacity:0.49;}.cls-2{fill:#3072b6;}
-svg {
-    height: 40px;
-    margin: 4px 8px;
-}</style>
