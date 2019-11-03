64ca5c2d8b9081bffb23cb5dde0ba64298236a96
Pablo Escodebar <escodebar@gmail.com>
2019-11-03 15:42:37 +0100

    Implement about

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,35 +1,36 @@
 <template>
-  <v-card class="pa-10">
-    <v-row justify="space-between">
-      <v-col cols="6">
-        <v-img
-          src="https://live.staticflickr.com/1901/43152721340_53b1fa14e0_b.jpg"
-        />
-      </v-col>
+  <v-container>
+    <v-card class="pa-10">
+      <v-img
+        src="https://github.com/escodebar/happy-frog/blob/master/architecture_happyfrog.png?raw=true"
+      />
 
-      <v-col cols="6">
-        <v-card-text>
-          A pond is an area filled with water, either natural or artificial,
-          that is smaller than a lake. It may arise naturally in floodplains as
-          part of a river system, or be a somewhat isolated depression (such as
-          a kettle, vernal pool, or prairie pothole). It may contain shallow
-          water with marsh and aquatic plants and animals.
-        </v-card-text>
+      <v-card-text>
+        This project was started to measure the water temperature and pH values
+        of the pond at the
+        <a href="https://www.herzogenmatt.ch/">Herzogenmatt natural reserve</a>.
+      </v-card-text>
 
-        <v-card-text>
-          Factors that affect the type of life found in a pond include depth and
-          duration of water level, nutrients, shade, presence or absence of
-          inlets and outlets, effects of grazing animals, and salinity.
-        </v-card-text>
+      <v-card-text>
+        Sensors are submerged into ponds to measure various properties of the
+        water such as pH and temperature. We use TTN community based LoRaWAN
+        network to transfer the data from the ponds into the cloud. We built our
+        own TTN Gateway to collect the data from the remote sensors. Sensors
+        closer to civilization can use standard WiFi to transfer the data. The
+        data is made publicly available through the openSenseMap, an open data
+        hosting provider. A web application gives an overview of the ponds and
+        gives access to the data and inform the user of how to contribute to
+        these measurements. The web application is hosted in the Google Cloud.
+        Continuous deployment is made with Travis upon changes on the public
+        Github repository.
+      </v-card-text>
 
-        <v-card-text>
-          Ponds are frequently man-made, or expanded beyond their original depth
-          and bounds. Among their many uses, ponds provide water for agriculture
-          and livestock, aid in habitat restoration, serve as fish hatcheries,
-          are components of landscape architecture, may store thermal energy as
-          solar ponds, and treat wastewater as treatment ponds.
-        </v-card-text>
-      </v-col>
-    </v-row>
-  </v-card>
+      <v-card-text>
+        To contribute to this project, please visit:
+        <a href="https://github.com/escodebar/happy-frog"
+          >https://github.com/escodebar/happy-frog</a
+        >
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
