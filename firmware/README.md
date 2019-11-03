ecb8b1c328ff7b29881bdeb233bda9646a43e2ac
YD95 <yannick.duc95@gmail.com>
2019-11-03 11:49:53 +0100

    Updated Firmware

--- firmware/README.md
+++ firmware/README.md
@@ -1,7 +1,9 @@
-# TTN TTGO LoRa32 
+# Firmware TTGO LoRa32 with TTN and Sensors
 
-Simple sketch, using PlatformIO IDE, to use the TTGO LoRa32 board as a node for The Things Network.
+Sketch, using PlatformIO IDE, to use the TTGO LoRa32 board as a node for The Things Network.
 
-The code uses IBM LMIC version for Arduino, but not streamlined for low memory devices.
+Communication with the DS18B20 teperatur sensor and a PH Prob is implemented.
+
+The code uses IBM LMIC version for Arduino that isn't streamlined for low memory devices.
 
 For more information goto:  https://wp.me/p8SNr-sR  -> Wordpress blog
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,7 +1,7 @@
-// MIT License
-// https://github.com/gonzalocasas/arduino-uno-dragino-lorawan/blob/master/LICENSE
+// GNU Affero General Public License v3.0
+// License https://github.com/escodebar/happy-frog/blob/master/LICENSE
 // Based on examples from https://github.com/matthijskooijman/arduino-lmic
-// Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+// Copyright (c) 2019 HappyFrog
 
 #include <Arduino.h>
 #include "lmic.h"
@@ -21,13 +21,40 @@ unsigned int counter = 0;
 
 SSD1306 display (OLED_I2C_ADDR, OLED_SDA, OLED_SCL);
 
+//Sensor
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial soft_serial;
+
+#define ph_serial soft_serial
+
+const uint8_t bufferlen = 32;                         //total buffer size for the response_data array
+char response_data[bufferlen]; 
+String inputstring = "";
+
+#define ONEWIRE_PIN 13
+OneWire oneWire(ONEWIRE_PIN);
+DallasTemperature ds18b20(&oneWire);
+float last_temp;
+
+// Data Variable for PH and Temp
+String STemp;
+String SPH;
+float TempMean = 0;
+float PHMean = 0;
+int8_t TempLora = 0;
+uint8_t PHLora = 0;
+
+
 /*************************************
  * TODO: Change the following keys
  * NwkSKey: network session key, AppSKey: application session key, and DevAddr: end-device address
  *************************************/
-static u1_t NWKSKEY[16] = {  ... };  // Paste here the key in MSB format
+static u1_t NWKSKEY[16] = { ... };  // Paste here the key in MSB format
 
-static u1_t APPSKEY[16] = { ...  };  // Paste here the key in MSB format
+static u1_t APPSKEY[16] = { ... };  // Paste here the key in MSB format
 
 static u4_t DEVADDR = 0x00000000;   // Put here the device id in hexadecimal form.
 
@@ -52,18 +79,23 @@ const lmic_pinmap lmic_pins = {
 
 void do_send(osjob_t* j){
     // Payload to send (uplink)
-    static uint8_t message[] = "{\"Temperature\":1}";
+    TempLora = (int8_t)TempMean;        // Teperatur range -127 ... 126
+    PHMean *= 10;
+    PHLora = (uint8_t)PHMean;           // PH range 0 ... 140 (devide by 10 for actual value)
+    uint8_t message[] = {TempLora,PHLora};   // I know sending a signed int with a unsigned int is not good.
 
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
         Serial.println(F("OP_TXRXPEND, not sending"));
     } else {
         // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, message, sizeof(message)-1, 0);
+        LMIC_setTxData2(1, message, sizeof(message), 0);
         Serial.println(F("Sending uplink packet..."));
         digitalWrite(LEDPIN, HIGH);
         display.clear();
         display.drawString (0, 0, "Sending uplink packet...");
+        display.drawString (0, 15, "Tp: " + String(TempMean) + " °C");    //displaying Temp Messurment Data
+        display.drawString (0, 30, "PH: " + String(PHMean));      //displaying PH Messurment Data as Decimal
         display.drawString (0, 50, String (++counter));
         display.display ();
     }
@@ -197,8 +229,59 @@ void setup() {
 
     // Start job
     do_send(&sendjob);
+
+//Sensor
+// put your setup code here, to run once:
+  // Serial.begin(115200);
+  ph_serial.begin(9600, 22, 17);
+  inputstring.reserve(20); 
+
+  ph_serial.print("*ok,0\r");
+  //pH_sensor.send_cmd_no_resp("c,0");       //send the command to turn off continuous mode
+                                           //in this case we arent concerned about waiting for the reply
+  // delay(100);
+  // pH_sensor.send_cmd_no_resp("*ok,0");     //send the command to turn off the *ok response
+                                           //in this case we wont get a reply since its been turned off
+  //delay(100);
+  //pH_sensor.flush_rx_buffer();   
+  // pinMode(16, OUTPUT);
+  // digitalWrite(16, HIGH);
+
+
 }
 
 void loop() {
     os_runloop_once();
+
+
+//Sensor
+delay(1000);
+  if (Serial.available() > 0) {                       //if we get data from the computer
+    inputstring = Serial.readStringUntil(13);                 //parse the data to either switch ports or send it to the circuit
+    if (inputstring != "") {                          //if we have a command for the modules
+      //pH_sensor.send_cmd(inputstring, response_data, bufferlen); // send it to the module of the port we opened
+      //Serial.println(response_data);                  //print the modules response
+      //response_data[0] = 0;                           //clear the modules response
+      Serial.println("send command: " + inputstring);
+      ph_serial.print(inputstring + "\r");
+      Serial.println("answer: " + ph_serial.readStringUntil(13));
+      // Serial.println(String(TempMean));
+    }
+  }
+  // pH_sensor.send_read();
+  ph_serial.print("r\r");                 // opens a connection to the Sensor
+  SPH = ph_serial.readStringUntil(13);    // only one read possible than cloesd untill its opened again
+  Serial.println("pH: " + SPH);
+  PHMean += SPH.toFloat();
+  PHMean /= 2;
+
+  ds18b20.requestTemperatures();
+  //for now, we want to read only the first sensor
+  last_temp = ds18b20.getTempCByIndex(0);
+  STemp = String(last_temp);
+  Serial.println("ds18b20: " + String(STemp) + "°C");
+  TempMean += STemp.toFloat();
+  TempMean /= 2;
+  Serial.println( String(PHMean) + " / " + String(TempMean));
+delay(1000);
 }
