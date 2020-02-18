9b62f81ffcb7939dd7b2d1a13dd4aa25bd5556f6
zdroyer <zdroyer@zdrenbook>
2020-02-18 19:36:49 +0100

    Added deepsleep between sending data. Confirmed working

--- sensenature-node/sensenature-node.cpp
+++ sensenature-node/sensenature-node.cpp
@@ -6,13 +6,13 @@
 
 #define LMIC_DEBUG_LEVEL 2
 
-
 #include <Arduino.h>
-#include <lmic.h>
-#include <lmic/lmic_bandplan_eu868.h>
-#include <lmic/lorabase_eu868.h>
-#include <hal/hal.h>
 #include <SPI.h>
+#include <FS.h>
+#include <SPIFFS.h>
+
+#include <lmic.h>
+#include <hal/hal.h>
 #include <heltec.h>
 #include "soc/efuse_reg.h"
 
@@ -73,6 +73,7 @@ void os_getDevKey (u1_t* buf) { }
 
 static osjob_t sendjob;
 
+
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
 const unsigned TX_INTERVAL = 60;
@@ -83,147 +84,103 @@ const lmic_pinmap lmic_pins = {
     .nss = 18,
     .rxtx = LMIC_UNUSED_PIN,
     .rst = 14,
-    .dio = {26, 34, 35 } // Pins for the Heltec ESP32 Lora board/ TTGO Lora32 with 3D metal antenna
+    .dio = {26, 34 , 35 }
 };
+
+unsigned long startTime, endTime;
+
+
+#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  30        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
+
+const char *  get_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+  static char sDefaultReason[30];
+
+  sprintf(sDefaultReason, "NotDS: %d\0", wakeup_reason);
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : return "DSWU:ext RTC_IO"; break;
+    case ESP_SLEEP_WAKEUP_EXT1 : return "DSWU:ext RTC_CNTL"; break;
+    case ESP_SLEEP_WAKEUP_TIMER : return "DSWU: timer"; break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : return "DSWU: touch"; break;
+    case ESP_SLEEP_WAKEUP_ULP : return "DSWU: ULP"; break;
+  }
+  return sDefaultReason;
+}
+
+
+
+void onSent(void *pUserData, int fSuccess){
+
+   // Heltec.display->clear();
+  Serial.write("Sending result: ");
+   if( fSuccess){
+	   Heltec.display->drawString (30, 0, "OK");
+	   Serial.println("OK");
+   } else {
+	   Heltec.display->drawString (30, 0, "FAILED");
+	   Serial.println("FAILED");
+   }
+   // Schedule next transmission
+   //os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+   digitalWrite(LEDPIN, LOW);
+   Heltec.display->drawString (0, 10, "Package# "+ String (bootCount));
+   Heltec.display->display ();
+
+   LMIC_shutdown();
+   unsigned long delta = millis() - startTime;
+   Serial.println("Work time: "+String(delta)+"ms");
+   Serial.println("Going to sleep now");
+   Serial.flush();
+
+   esp_deep_sleep_start();
+
+    // Schedule next transmission
+    //os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+
+}
 
 void do_send(osjob_t* j){
     // Payload to send (uplink)
-	TempMean = 18.3;
+    Heltec.display->clear();
+    Serial.print("Wake up #");
+    Serial.print(bootCount);
+    Serial.print(", reason: ");
+    Serial.println(get_wakeup_reason());
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR - 2228000);
+    //2228ms is the average work time (10 samplese)
+
+    //Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + "s");
+
+	uint8_t serialNo = 0x01;
+	TempMean = 18.31;
     TempMean *= 100;
     TempLora = (int16_t)TempMean;        // Teperatur range -127.00 ... 126.00
     TempLoraHigh = TempLora >> 8;
     TempLoraLow = TempLora & 0x00FF;
-    PHMean = 5.53;
+    PHMean = 5.54;
     PHMean *= 100;
     PHLora = (uint16_t)PHMean;           // PH range 0 ... 140 (devide by 10 for actual value)
     PHLoraHigh = PHLora >> 8;
     PHLoraLow = PHLora & 0x00FF;
-    uint8_t message[] = {TempLoraHigh,TempLoraLow,PHLoraHigh,PHLoraLow};   // I know sending a signed int with a unsigned int is not good.
+    uint8_t message[] = {TempLoraHigh,TempLoraLow,PHLoraHigh,PHLoraLow /*, serialNo*/};   // I know sending a signed int with a unsigned int is not good.
 
-    // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
-    } else {
-        // Prepare upstream data transmission at the next possible time.
-        lmic_tx_error_t ret = LMIC_setTxData2(1, message, sizeof(message), 0);
-        Serial.println("Sending uplink packet..., returned: "+String(ret));
-        digitalWrite(LEDPIN, HIGH);
-        Heltec.display->clear();
-        Heltec.display->drawString (0, 0, "SND...");
-        //Heltec.display->drawString (0, 15, "Tp: " + String(TempMean) + " °C");    //displaying Temp Messurment Data
-        //Heltec.display->drawString (0, 30, "PH: " + String(PHMean));      //displaying PH Messurment Data as Decimal
-        Heltec.display->drawString (0, 10,"# "+String (counter));
-        Heltec.display->display ();
-        counter++;
-       // os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-    }
-    // Next TX is scheduled after TX_COMPLETE event.
+	// Prepare upstream data transmission at the next possible time.
+	lmic_tx_error_t ret = LMIC_sendWithCallback(1, message, sizeof(message), 0, onSent, (void*)0);
+	if( ret != LMIC_ERROR_SUCCESS ){
+		Serial.println("Cannot register sending the LoRaWAN package. Error = "+String(ret));
+	} else {
+		digitalWrite(LEDPIN, HIGH);
+		Serial.println("Sending uplink packet.");
+		Heltec.display->clear();
+		Heltec.display->drawString (0, 0, "SND...");
+		Heltec.display->display ();
+	}
 }
-
-void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
-    switch(ev) {
-        case EV_SCAN_TIMEOUT:
-            Serial.println(F("EV_SCAN_TIMEOUT"));
-            break;
-        case EV_BEACON_FOUND:
-            Serial.println(F("EV_BEACON_FOUND"));
-            break;
-        case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
-            break;
-        case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
-            break;
-        case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
-            break;
-        case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
-            break;
-        /*
-        || This event is defined but not used in the code. No
-        || point in wasting codespace on it.
-        ||
-        || case EV_RFU1:
-        ||     Serial.println(F("EV_RFU1"));
-        ||     break;
-        */
-        case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
-            break;
-        case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
-            break;
-        case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack"));
-            if (LMIC.dataLen) {
-              Serial.println(F("Received "));
-              Serial.println(LMIC.dataLen);
-              Serial.println(F(" bytes of payload"));
-            }
-
-            Heltec.display->clear();
-                   Heltec.display->drawString (0, 0, "SND done!");
-                   Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-                   if (LMIC.txrxFlags & TXRX_ACK) {
-                     Serial.println(F("Received ack"));
-                     Heltec.display->drawString (0, 20, "Got ACK");
-                   } else {
-                       Serial.println(F("NO ACK received!"));
-                       Heltec.display->drawString (0, 20, "No ACK.");
-                     }
-                   // Schedule next transmission
-                   os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-                   digitalWrite(LEDPIN, LOW);
-                   Heltec.display->drawString (0, 10, "## "+ String (counter));
-                   Heltec.display->display ();
-
-
-            // Schedule next transmission
-            //os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-            break;
-        case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
-            break;
-        case EV_RESET:
-            Serial.println(F("EV_RESET"));
-            break;
-        case EV_RXCOMPLETE:
-            // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
-            break;
-        case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
-            break;
-        case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
-            break;
-        /*
-        || This event is defined but not used in the code. No
-        || point in wasting codespace on it.
-        ||
-        || case EV_SCAN_FOUND:
-        ||    Serial.println(F("EV_SCAN_FOUND"));
-        ||    break;
-        */
-        case EV_TXSTART:
-            Serial.println(F("EV_TXSTART"));
-            break;
-        default:
-            Serial.print(F("Unknown event: "));
-            Serial.println((unsigned) ev);
-            break;
-    }
-}
-
-
-
-
-
 
 
 int getChipRevision()
@@ -231,65 +188,21 @@ int getChipRevision()
   return (REG_READ(EFUSE_BLK0_RDATA3_REG) >> (EFUSE_RD_CHIP_VER_REV1_S)&&EFUSE_RD_CHIP_VER_REV1_V) ;
 }
 
-void printESPRevision() {
-  Serial.print("REG_READ(EFUSE_BLK0_RDATA3_REG): ");
-  Serial.println(REG_READ(EFUSE_BLK0_RDATA3_REG), BIN);
-
-  Serial.print("EFUSE_RD_CHIP_VER_REV1_S: ");
-  Serial.println(EFUSE_RD_CHIP_VER_REV1_S, BIN);
-
-  Serial.print("EFUSE_RD_CHIP_VER_REV1_V: ");
-  Serial.println(EFUSE_RD_CHIP_VER_REV1_V, BIN);
-
-  Serial.println();
-
-  Serial.print("Chip Revision (official version): ");
-  Serial.println(getChipRevision());
-
-  Serial.print("Chip Revision from shift Operation ");
-  Serial.println(REG_READ(EFUSE_BLK0_RDATA3_REG) >> 15, BIN);
-
-}
-
 
 void setup() {
+	startTime = millis();
     Heltec.begin(true, false);
+    delay(1500);
+    bootCount++;
     // LMIC init
-     os_init();
-
+    os_init_ex(&lmic_pins);
      // Reset the MAC state. Session and pending data transfers will be discarded.
-     LMIC_reset();
-     LMIC_setClockError(MAX_CLOCK_ERROR * 3 / 100);
+	LMIC_reset();
+	LMIC_setClockError(MAX_CLOCK_ERROR * 3 / 100);
+	LMIC_setSession (0x13, DEVADDR, NWKSKEY, APPSKEY);
+	LMIC_setSeqnoUp(bootCount);
 
-///	Serial.begin(115200);
-    delay(1500);   // Give time for the seral monitor to start up
-    Serial.println(F("Starting..."));
-
-
-    printESPRevision();
-
-    // Use the Blue pin to signal transmission.
-    //pinMode(LEDPIN,OUTPUT);
-
-   // reset the OLED
-///   pinMode(OLED_RESET,OUTPUT);
-///   digitalWrite(OLED_RESET, LOW);
-///   delay(50);
-///   digitalWrite(OLED_RESET, HIGH);
-
-    ///Heltec.display->init ();
-    ///Heltec.display->flipScreenVertically ();
-    ///Heltec.display->setFont (ArialMT_Plain_10);
-
-    ///Heltec.display->setTextAlignment (TEXT_ALIGN_LEFT);
-
-    ///Heltec.display->drawString (0, 0, "Init!");
-    ///Heltec.display->display ();
-
-
-
-
-    LMIC_setSession (0x13, DEVADDR, NWKSKEY, APPSKEY);
+	//(bootCount);
 
     // Set up the channels used by the Things Network, which corresponds
     // to the defaults of most gateways. Without this, only three base
@@ -333,10 +246,6 @@ void setup() {
     LMIC_setDrTxpow(DR_SF7,14);
     LMIC_startJoining();
 
-
-    // Start job
-    do_send(&sendjob);
-
 //Sensor
 // put your setup code here, to run once:
   // Serial.begin(115200);
@@ -354,22 +263,16 @@ void setup() {
   // pinMode(16, OUTPUT);
   // digitalWrite(16, HIGH);
 
+    // Start job
+    do_send(&sendjob);
 
 }
 
+
 void loop() {
 
-
-    unsigned long now;
-      now = millis();
-      if ((now & 512) != 0) {
-        digitalWrite(13, HIGH);
-      }
-      else {
-        digitalWrite(13, LOW);
-      }
-
-      os_runloop_once();
+	//will not be called after the deepsleep wake up
+     os_runloop_once();
 
 //Sensor
 // delay(1000);
@@ -401,7 +304,7 @@ if (Serial.available() > 0) {                       //if we get data from the co
 
   /*
   STemp = String(last_temp);
-  Serial.println("ds18b20: " + String(STemp) + "°C");
+  Serial.println("ds18b20: " + String(STemp) + "ï¿½C");
   TempMean += STemp.toFloat();
   TempMean /= 2;
   Serial.println( String(PHMean) + " / " + String(TempMean));
